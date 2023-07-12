bae2b11cdd8eaab4364ab3adedf311a9c3b69561
Leonardo Reis <dev.leonardo.reis@gmail.com>
2023-07-11 21:34:39 -0300

    endpoint para busca de id

--- src/main/java/br/com/projeto/dslist/services/GameService.java
+++ src/main/java/br/com/projeto/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.projeto.dslist.dto.GameDTO;
 import br.com.projeto.dslist.dto.GameMinDTO;
 import br.com.projeto.dslist.entities.Game;
 import br.com.projeto.dslist.repositories.GameRepository;
@@ -15,9 +17,16 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
     return result.stream().map(x -> new GameMinDTO(x)).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long id) {
+    Game result = gameRepository.findById(id).get();
+    return new GameDTO(result);
   }
 
 }
